0455eb50306f84006bdd3a788e91d93ae756e987
joshbrusa <joshbrusa@gmail.com>
2022-10-10 19:07:58 -0700

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import Header from "../components/Header";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
