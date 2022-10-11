d739af16a8018c0f2765fcdc88c70669edcc23d8
joshbrusa <joshbrusa@gmail.com>
2022-10-10 19:36:53 -0700

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import Header from "../components/Header";
 import type { AppProps } from "next/app";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-export default function Index() {
+export default function Page() {
   return (
     <>
       <div className=""></div>
