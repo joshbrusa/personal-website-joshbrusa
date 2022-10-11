78c2cd0d90142b2ddad9ce3e318cd45853681db4
joshbrusa <joshbrusa@gmail.com>
2022-10-10 19:17:35 -0700

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import "../styles/globals.css";
-import Header from "../components/Header";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <div className="flex justify-center">
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   );
 }
