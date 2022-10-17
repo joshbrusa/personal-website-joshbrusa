eecec09d4a59da6cf1b0b429966d8da2d5551040
joshbrusa <joshbrusa@gmail.com>
2022-10-16 20:00:51 -0700

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,11 @@ import Header from "../components/Header";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  if (router.pathname === "/") {
-    return (
-      <>
-        <div className="flex flex-col items-center">
-          <Component {...pageProps} />
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       <Header />
       <div className="flex flex-col items-center">
-        <div className="w-1/2 mt-2">
+        <div className="p-2 max-w-6xl">
           <Component {...pageProps} />
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Page() {
   return (
@@ -7,17 +6,7 @@ export default function Page() {
       <Head>
         <title>joshbrusa.com</title>
       </Head>
-      <div className="flex flex-col h-screen w-screen items-center justify-center">
-        <div className="text-8xl font-sacramento">joshbrusa.com</div>
-        <div className="text-2xl">A portfolio website made by yours truly.</div>
-        <Link href={"/resume"}>
-          <a className="mt-20 index-link">Resume</a>
-        </Link>
-        <Link href={"/showcase"}>
-          <a className="mt-4 index-link">Showcase</a>
-        </Link>
-        <div className="mt-20 text-4xl">Contact: joshbrusa@gmail.com</div>
-      </div>
+      <div className="page-title">A portfolio website made by yours truly.</div>
     </>
   );
 }
