39ab3a52a5a75b285689757ee0414d015ab7c7c1
joshbrusa <joshbrusa@gmail.com>
2022-10-18 20:10:26 -0700

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
     <>
       <Header />
       <div className="flex flex-col items-center">
-        <main className="p-2 max-w-6xl overflow-auto">
+        <main className="p-2 max-w-6xl">
           <Component {...pageProps} />
         </main>
       </div>
