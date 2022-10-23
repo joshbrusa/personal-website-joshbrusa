a4d5d3275440ad365dab676a86b6a0aa8d2dbd2f
joshbrusa <joshbrusa@gmail.com>
2022-10-23 14:33:54 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
       >
         <div className="p-2 flex items-center justify-between shadow-lg rounded-lg">
           <Link href={"/"}>
-            <a className="m-2 text-4xl font-Bangers text-theme hover:underline">
+            <a className="text-4xl font-Bangers text-theme hover:underline">
               Josh Brusa
             </a>
           </Link>
@@ -38,7 +38,7 @@ export default function Header() {
 
         <div className="mt-2 flex justify-end">
           <div
-            className={`p-2 absolute flex flex-col justify-end w-60 shadow-lg rounded-lg bg-bg ${
+            className={`absolute flex flex-col justify-end w-60 shadow-lg rounded-lg bg-bg ${
               menuOpen ? null : "hidden"
             }`}
           >
