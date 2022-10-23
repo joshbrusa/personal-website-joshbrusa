d7c90387251672a14442e2502f007661af33b72e
joshbrusa <joshbrusa@gmail.com>
2022-10-23 14:39:16 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,61 +17,59 @@ export default function Header() {
 
   return (
     <>
-      <div
-        onMouseLeave={() => {
-          setMenuOpen(false);
-          setAboutOpen(false);
-          setProjectsOpen(false);
-        }}
-      >
-        <div className="p-2 flex items-center justify-between shadow-lg rounded-lg">
-          <Link href={"/"}>
-            <a className="text-4xl font-Bangers text-theme hover:underline">
-              Josh Brusa
-            </a>
-          </Link>
+      <div className="p-2 flex items-center justify-between shadow-lg rounded-lg">
+        <Link href={"/"}>
+          <a className="text-4xl font-Bangers text-theme hover:underline">
+            Josh Brusa
+          </a>
+        </Link>
 
-          <button onClick={() => setMenuOpen(!menuOpen)} className="w-10 h-10">
-            <Bars3BottomRightIcon />
-          </button>
-        </div>
+        <button onClick={() => setMenuOpen(!menuOpen)} className="w-10 h-10">
+          <Bars3BottomRightIcon />
+        </button>
+      </div>
 
-        <div className="mt-2 flex justify-end">
-          <div
-            className={`absolute flex flex-col justify-end w-60 shadow-lg rounded-lg bg-bg ${
-              menuOpen ? null : "hidden"
-            }`}
+      <div className="mt-2 flex justify-end">
+        <div
+          className={`absolute flex flex-col justify-end w-60 shadow-lg rounded-lg bg-bg ${
+            menuOpen ? null : "hidden"
+          }`}
+        >
+          <button
+            onClick={() => {
+              setAboutOpen(!aboutOpen);
+              setProjectsOpen(false);
+            }}
+            className="menu-folder"
           >
-            <button
-              onClick={() => setAboutOpen(!aboutOpen)}
-              className="menu-folder"
-            >
-              About
-            </button>
-            <div className={`flex flex-col ${aboutOpen ? null : "hidden"}`}>
-              <Link href={"/about/experience"}>
-                <a className="menu-link">Experience</a>
-              </Link>
-              <Link href={"/about/education"}>
-                <a className="menu-link">Education</a>
-              </Link>
-              <Link href={"/about/contact"}>
-                <a className="menu-link">Contact</a>
-              </Link>
-            </div>
-            <button
-              onClick={() => setProjectsOpen(!projectsOpen)}
-              className="menu-folder"
-            >
-              Projects
-            </button>
-            <div className={`flex flex-col ${projectsOpen ? null : "hidden"}`}>
-              <Link href={"https://joshbrusa-stonks.vercel.app/"}>
-                <a target="_blank" className="menu-link">
-                  Stonks
-                </a>
-              </Link>
-            </div>
+            About
+          </button>
+          <div className={`flex flex-col ${aboutOpen ? null : "hidden"}`}>
+            <Link href={"/about/experience"}>
+              <a className="menu-link">Experience</a>
+            </Link>
+            <Link href={"/about/education"}>
+              <a className="menu-link">Education</a>
+            </Link>
+            <Link href={"/about/contact"}>
+              <a className="menu-link">Contact</a>
+            </Link>
+          </div>
+          <button
+            onClick={() => {
+              setProjectsOpen(!projectsOpen);
+              setAboutOpen(false);
+            }}
+            className="menu-folder"
+          >
+            Projects
+          </button>
+          <div className={`flex flex-col ${projectsOpen ? null : "hidden"}`}>
+            <Link href={"https://joshbrusa-stonks.vercel.app/"}>
+              <a target="_blank" className="menu-link">
+                Stonks
+              </a>
+            </Link>
           </div>
         </div>
       </div>
